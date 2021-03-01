20e25cf5cfd89a7fb9a06eb78a69fa216a6b318f
Josh Frank <eurosewusa@gmail.com>
2021-02-28 21:03:21 -0500

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -8,24 +8,33 @@ function App() {
 
   const [ dogs, setDogs ] = useState( [] );
   const [ currentDog, setCurrentDog ] = useState( {} );
+  const [ filterStatus, setFilterStatus ] = useState( false );
 
   useEffect( () => {
     fetch( API ).then( response => response.json() ).then( setDogs );
   }, [] );
 
-  // console.log( currentDog );
+  // console.log( filterStatus );
 
   return (
     <div className="App">
       <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
+        <button id="good-dog-filter" onClick={ () => setFilterStatus( filterStatus => !filterStatus ) }>
+          Filter good dogs: { filterStatus ? "ON" : "OFF" }
+        </button>
       </div>
       <div id="dog-bar">
-        <DogBar dogs={ dogs } setCurrentDog={ setCurrentDog }/>
+        <DogBar dogs={ dogs.filter( dog => filterStatus ? dog.isGoodDog === true : true ) } setCurrentDog={ setCurrentDog }/>
       </div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
-        <DogView dogs={ dogs } setDogs={ setDogs } currentDog={ currentDog } setCurrentDog={ setCurrentDog } apiUrl={ API }/>
+        <DogView
+          dogs={ dogs }
+          setDogs={ setDogs }
+          currentDog={ currentDog }
+          setCurrentDog={ setCurrentDog }
+          apiUrl={ API }
+        />
       </div>
     </div>
   );
